7553e9edd73e06702ad09b8da8594359c7f784b6
01000110010 <jrkc81@gmx.com>
2024-04-17 11:47:18 +0100

    Add files via upload
    CustomerService and Chat services where added askQuestion ProcessCheckout Chat
    implemented new functions for askQuestion processCheckout and verifyCard
    added new services togRPC server StockControl chatservice CustomerService, Checkout, CheckoutChat, and CustomerServiceChat
    Improved error handling for logging and handling of errors during card verification process
    now use bidirectional streaming for checkoutporcess
    added better commenting for code and formatted for better readability

--- server.js
+++ server.js
@@ -1,102 +1,161 @@
-//imports the @grpc/grpc-js package, which provides the functionality for working with gRPC
+//load necessary modules
 const grpc = require('@grpc/grpc-js');
-//imports the @grpc/proto-loader package, which is used to load Protocol Buffer in gRPC apps
 const protoLoader = require('@grpc/proto-loader');
-//import the file system module
-const fs = require('fs'); 
-//import the path module
-const path = require('path'); 
-//loads the Protocol Buffer definition file named StockControl.proto
-const packageDefinition = protoLoader.loadSync('StockControl.proto', {});
-//loads the Protocol Buffer definition file named Checkout.proto
-const checkoutPackageDefinition = protoLoader.loadSync('Checkout.proto', {});
-//loads the gRPC package definition from the packageDefinition
+const fs = require('fs');
+const path = require('path');
+
+//load Protocol Buffer definitions for StockControl Checkout and CustomerService services
+const packageDefinition = protoLoader.loadSync('StockControl.proto');
+const checkoutPackageDefinition = protoLoader.loadSync('Checkout.proto');
+const customerServicePackageDefinition = protoLoader.loadSync('CustomerService.proto');
+
+//load gRPC package definitions
 const smartretail = grpc.loadPackageDefinition(packageDefinition).smartretail;
-const checkoutPackage = grpc.loadPackageDefinition(checkoutPackageDefinition).smartretail;//added
+const checkoutPackage = grpc.loadPackageDefinition(checkoutPackageDefinition).smartretail;
+const customerServicePackage = grpc.loadPackageDefinition(customerServicePackageDefinition).smartretail;
 
-//implemtaion for adding products
-//function that takes parameters call and callback represents logic for adding products
-const addProduct = (call, callback) => {
-  //logs product information received via call.request
-  console.log('Adding product:', call.request);
-  callback(null, { id: call.request.id, message: 'Product added successfully' });
+//define opening times for customer service
+const customerServiceOpeningTimes = "9am to 9pm";
+//define the delivery cost
+const deliveryCost = "5.99"; 
 
-  //prepare the product data string to write to the file
-  const productData = `Product Added - ID: ${call.request.id}, Name: ${call.request.name}, Price: ${call.request.price}, Quantity: ${call.request.quantity}\n`;
-  //filepath to create the file
-  const filePath = path.join('C:/Users/cj13y/Documents/hdip_2023_ncirl/distrubited_systems/x14115034_Cullen_Janni', 'products.txt');
-  fs.appendFile(filePath, productData, (err) => {
-    if (err) {
-      console.error('Error writing product to file:', err);
-    } else {
-      console.log('Product data added to file successfully');
-    }
-  });
-};
-
-//creates a new gRPC server instance
-const server = new grpc.Server();
-
-//adds services to the gRPC server
-server.addService(smartretail.StockControl.service, {
-  AddProduct: addProduct
-});
-
-
-//function for calculating total cost of products in shopping cart
-const calculateTotal = (call, callback) => {
-  const products = call.request.items;
-
-  if (!products || products.length === 0) {
-    const error = new Error('No products found in the cart');
-    callback(error, null);
-    return;
-  }
-
-  let total = 0;
-  products.forEach(product => {
-    total += product.price * product.quantity;
-  });
-  //logs total cost
-  console.log("The total cost of your cart is: \u20AC" + total.toFixed(2));
-
-  callback(null, { total });
-};
-
-
-//implementaion for verifycard 
-function verifycard(call, callback){
-  const { cardNumber, expiryDate, cvv } = call.request;
-  //credit/debit card validation logic
-  const cardNumberIsValid = cardNumber.length >=16;
-  const expiryDateIsValid = /^(0[1-9]|1[0-2])\/[0-9]{2}$/.test(expiryDate);
-  const cvvIsValid = cvv.length === 3;
-
-  if (cardNumberIsValid && expiryDateIsValid && cvvIsValid) {
-      console.log("Credit/debit card details are valid.");
-      //conitune with processing
-  } else {
-      console.log("Invalid credit/debit card details have been entered. Please check again");
-  }
-  callback(null, { isValid: true, message: "Card is valid" });
+//implementation for adding products
+function addProduct(call, callback) {
+    console.log('Adding product:', call.request);
+    //log product information received via the call
+    const productData = `Product Added - ID: ${call.request.id}, Name: ${call.request.name}, Price: ${call.request.price}, Quantity: ${call.request.quantity}\n`;
+    const filePath = path.join(__dirname, 'products.txt');
+    fs.appendFile(filePath, productData, (err) => {
+        if (err) {
+            console.error('Error writing product to file:', err);
+            //send error response to client
+            callback(err, null);
+        } else {
+            console.log('Product data added to file successfully');
+            //send success response to client
+            callback(null, { id: call.request.id, message: 'Product added successfully' });
+        }
+    });
 }
 
+//implementation for bidirectional streaming RPC for processing checkout
+function processCheckout(call) {
+    let cardValid = false;
 
-//checkout service to the gRPC server
-server.addService(checkoutPackage.Checkout.service, {
-  CalculateTotal: calculateTotal,
-  VerifyCard: verifycard,
+    call.on('data', function(request) {
+        const { cart, paymentInfo } = request;
+
+        verifyCard(paymentInfo, (error, response) => {
+            if (error) {
+                console.error('Error verifying card:', error.message);
+                return;
+            }
+            cardValid = response.isValid;
+            //send response based on card validity
+            if (cardValid) {
+                let total = 0;
+                cart.items.forEach(item => {
+                    total += item.price * item.quantity;
+                });
+                //send response with total if card is valid
+                call.write({ totalResponse: { total: total }, cardVerificationResponse: { isValid: cardValid } });
+            } else {
+                //send response indicating card is invalid
+                call.write({ cardVerificationResponse: { isValid: cardValid, message: "Invalid credit/debit card details have been entered. Please check again" } });
+            }
+        });
+    });
+    //client has stopped
+    console.log('Client finished sending data.');
+    call.on('end', function() {
+        call.end();
+    });
+}
+
+//implementation for verifying credit/debit cards
+function verifyCard(paymentInfo, callback) {
+    const { cardNumber, expiryDate, cvv } = paymentInfo;
+    const cardNumberIsValid = cardNumber.length >= 16;
+    const expiryDateIsValid = /^(0[1-9]|1[0-2])\/[0-9]{2}$/.test(expiryDate);
+    const cvvIsValid = cvv.length === 3;
+
+    if (cardNumberIsValid && expiryDateIsValid && cvvIsValid) {
+        console.log("Credit/debit card details are valid.");
+        //send success response to client
+        callback(null, { isValid: true, message: "Card is valid" });
+    } else {
+        console.log("Invalid credit/debit card details have been entered. Please check again");
+        //send failure response to client
+        callback(null, { isValid: false, message: "Card is invalid" });
+    }
+}
+
+//implementation for bidirectional streaming RPC for chat
+function chat(call) {
+    call.on('data', function(chatMessage) {
+        console.log(`Received message from ${chatMessage.user}: ${chatMessage.message}`);
+        //respond back to the client with an acknowledgment
+        call.write({ user: 'Server', message: `Message received: ${chatMessage.message}` });
+    });
+
+    call.on('end', function() {
+        call.end();
+    });
+}  
+
+//implementation for customerService service
+function askQuestion(call, callback) {
+    const { questionType } = call.request;
+
+    switch (questionType) {
+        //question about delivery  
+        case 1: 
+            callback(null, { message: `The delivery cost is ${deliveryCost}.` });
+            break;
+            //question about opening hours
+        case 2: 
+            callback(null, { message: `Customer service is open ${customerServiceOpeningTimes}.` });
+            break;
+        default:
+            callback({ code: grpc.status.INVALID_ARGUMENT, message: 'Invalid question type.' });
+    }
+}
+
+//create gRPC server instance
+const server = new grpc.Server();
+
+//add services to the gRPC server
+server.addService(smartretail.StockControl.service, {
+    AddProduct: addProduct
 });
 
-//port number server will listen to
+server.addService(smartretail.ChatService.service, { 
+    Chat: chat 
+});
+
+server.addService(checkoutPackage.Checkout.service, {
+    ProcessCheckout: processCheckout
+});
+
+server.addService(customerServicePackage.CustomerService.service, {
+    AskQuestion: askQuestion,
+    Chat: chat 
+});
+server.addService(checkoutPackage.CheckoutChat.service, {
+    Chat: chat
+});
+server.addService(customerServicePackage.CustomerServiceChat.service, {
+    Chat: chat 
+});
+
+//define port number
 const port = '50051';
-//binds server to port
+
+//bind server to port
 server.bindAsync(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure(), (error, port) => {
-  //returns error 
-  if (error) {
-    console.error('Server error:', error);
-    return;
-  }
-  //connected successfully to port
-  console.log(`Server running at http://0.0.0.0:${port}`);
+    if (error) {
+        console.error('Server error:', error);
+        return;
+    }
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
